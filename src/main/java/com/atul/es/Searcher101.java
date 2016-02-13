7336868d027febc4227058215771f983a299db16
atulsm <atulsm@gmail.com>
2016-02-13 17:02:40 +0530

    A basic search client for es

---
+++ src/main/java/com/atul/es/Searcher101.java
@@ -0,0 +1,42 @@
+package com.atul.es;
+
+import java.util.Map;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+
+/**
+ * 
+ * @author satul
+ *
+ */
+public class Searcher101 {
+	public static void main(String[] args) throws Exception{	
+		TransportClient transportClient = new TransportClient();
+		transportClient = transportClient.addTransportAddress(new InetSocketTransportAddress(Configuration.IP, Configuration.PORT));			
+		Client client = (Client) transportClient;
+		System.out.println("Connected to elasticsearch");
+				
+		//Get all Data
+		SearchResponse response = client.prepareSearch(Configuration.INDEX).setQuery(QueryBuilders.matchAllQuery()).execute().actionGet();        
+		
+		//Query by term
+		//SearchResponse response = client.prepareSearch(Configuration.INDEX).setQuery(QueryBuilders.termQuery("pos", "1")).execute().actionGet();  
+		
+		SearchHit[] results = response.getHits().getHits();
+        System.out.println("Got " + results.length + " hits") ;
+        for (SearchHit hit : results) {
+            Map<String,Object> result = hit.getSource();   
+            System.out.println(result);
+        }
+	
+		// on shutdown
+		client.close();
+		transportClient.close();
+	}
+	
+}
