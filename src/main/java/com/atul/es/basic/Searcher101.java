ba0975a9005dd503cc4e1d00674a6a0ec9b56480
Atul Soman <atulsm@gmail.com>
2021-06-13 11:43:53 +0530

    enhancing query

--- src/main/java/com/atul/es/basic/Searcher101.java
+++ src/main/java/com/atul/es/basic/Searcher101.java
@@ -7,9 +7,12 @@ import java.util.stream.Collectors;
 import com.alibaba.fastjson.JSON;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 
@@ -26,7 +29,14 @@ public class Searcher101 {
 
 		System.out.println("Connected to elasticsearch");
 
+		SearchSourceBuilder builder = new SearchSourceBuilder()
+				.postFilter(QueryBuilders.rangeQuery("age").from(0).to(100));
+
 		SearchRequest searchRequest = new SearchRequest(Configuration.INDEX);
+		searchRequest.searchType(SearchType.DFS_QUERY_THEN_FETCH);
+		searchRequest.source(builder);
+
+
 		SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
 		SearchHit[] searchHits = response.getHits().getHits();
 		List<Person> results =
